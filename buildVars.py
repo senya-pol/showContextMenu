5f1b2172bfd2203f76a4a1429d3441a2e8261a0a
senya-pol <senya-pol@yandex-team.ru>
2025-11-18 22:39:54 +0300

    Update buildVars.py

--- buildVars.py
+++ buildVars.py
@@ -1,3 +1,19 @@
+# ShowContextMenu NVDA Add-on
+# Copyright (C) 2024 Arseniy Polyakov
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.
+
 # Build customizations
 # Change this file instead of sconstruct or manifest files, whenever possible.
 
@@ -105,3 +121,4 @@ brailleTables: BrailleTables = {}
 # displayName (name of the speech dictionary shown to users and translatable),
 # mandatory (True when always enabled, False when not.
 symbolDictionaries: SymbolDictionaries = {}
+
